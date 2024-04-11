b76d42a5ad7790bcfb9ba5d4818f9a68e9a9d293
Tomas Trujillo <tomastru114@gmail.com>
2024-04-11 18:36:00 +0200

    - Finish touching up final details

--- client/src/app/home/page.jsx
+++ client/src/app/home/page.jsx
@@ -3,6 +3,24 @@ import axios from "axios"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
+import { differenceInDays, differenceInHours, differenceInMinutes } from 'date-fns';
+
+const getElapsedTime = (createdAt) => {
+    const createdDate = new Date(createdAt);
+    const currentDate = new Date();
+
+    const daysDiff = differenceInDays(currentDate, createdDate);
+    const hoursDiff = differenceInHours(currentDate, createdDate);
+    const minutesDiff = differenceInMinutes(currentDate, createdDate);
+
+    if (daysDiff > 0) {
+        return `${daysDiff} día${daysDiff > 1 ? 's' : ''} atrás`;
+    } else if (hoursDiff > 0) {
+        return `${hoursDiff} hora${hoursDiff > 1 ? 's' : ''} atrás`;
+    } else {
+        return `${minutesDiff} minuto${minutesDiff > 1 ? 's' : ''} atrás`;
+    }
+};
 
 
 export default function UserPage() {
@@ -34,24 +52,25 @@ export default function UserPage() {
                 <div className="container mt-5 flex flex-col justify-center items-center">
                     <div className="bg-white border-4 radius rounded-xl sm:w-4/6 md:w-3/6 lg:w-2/6 flex flex-col sm:flex-row justify-evenly m-5">
                         <article>
-                            <h3>{groups.user}</h3>
+                            <h3 className="uppercase">{groups.user}</h3>
                         </article>
                     </div>
                     {groups.data && groups.data.map(group => (
                         <div key={group.name} className="border-4 radius rounded-xl sm:w-4/6 md:w-3/6 lg:w-2/6 flex flex-col sm:flex-row justify-evenly m-5" style={{ backgroundColor: "rgba(255, 255, 255, 0.4)" }}>
                             <Link key={group.name} href={`/home/${group.name}`}>
-
                                 <article>
                                     <h3>{group.name}</h3>
                                 </article>
+                                <hr />
                                 <article>
-                                    <span>{group.createdBy}</span>
-                                    <span>{group.createdAt}</span>
+                                    <span>Created By: {group.createdBy}</span>
+                                    <br />
+                                    <span>Creado {getElapsedTime(group.createdAt)}</span>
                                 </article>
-
                             </Link>
                         </div>
                     ))}
+
                     <section className="flex justify-center align-middle">
                         <div className="border-4 radius rounded-xl sm:w-4/6 md:w-3/6 lg:w-2/6 flex flex-col sm:flex-row justify-evenly m-5"
                             style={{ backgroundColor: "rgba(255, 255, 255, 0.6)" }}>
--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -2,19 +2,22 @@
 
 import Image from 'next/image';
 import { Button } from "@/components/ui/button"
+import Link from 'next/link';
 
 
 export default function Home() {
   return (
     <div className="h-[calc(100vh-5rem)]">
-      
-        <div className="inset-0 flex items-center justify-center text-white text-4xl font-bold z-20 flex-col h-full p-5">
-          <h4 className="text-center mb-10 font-bold text-[3vh]">
-            LLeva tu contador de muertes en tus juegos <br /> solo o con tu grupo de amigos
-          </h4>
+
+      <div className="inset-0 flex items-center justify-center text-white text-4xl font-bold z-20 flex-col h-full p-5">
+        <h4 className="text-center mb-10 font-bold text-[3vh]">
+          LLeva tu contador de muertes en tus juegos <br /> solo o con tu grupo de amigos
+        </h4>
+        <Link href="/auth" >
           <Button size="lg" variant="outline" className="text-black text-xl font-semibold ">Empieza ahora</Button>
-        </div>
+        </Link>
       </div>
-    
+    </div>
+
   );
 }
